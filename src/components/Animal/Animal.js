aaf47b042878ebee7a618ff0438e003290cb9c47
brenley-woodard <brenleyew@gmail.com>
2023-01-09 15:49:52 -0800

    animals are rendering to page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,7 +1,12 @@
-export default function Animal() {
+import './Animal.css';
+
+export default function Animal(props) {
+  console.log(props);
   return (
-    <div className="name">
-      <img></img>
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`}/>
+      <p className="name">{props.name}</p>
+      <p>{props.says}</p>
     </div>
   );
 }
